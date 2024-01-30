f37b5544f8f04daef55234c435e39b7bbb27dbf9
aybkh <a.khalifi@sapalomera.cat>
2024-01-30 17:57:28 +0100

    1

--- ErAySlasher/Assets/Scripts/DispararBalas.cs
+++ ErAySlasher/Assets/Scripts/DispararBalas.cs
@@ -4,20 +4,12 @@ using UnityEngine;
 
 public class DispararBalas : MonoBehaviour
 {
-<<<<<<< HEAD
     public GameObject Bala;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreaBala", 0f, 2f);
     }
-=======
-    public GameObject Bala; // El prefab de la bala
-    public Transform Enemic1, Enemic2; // El objeto al que se disparará
-
-    public float fireRate = 1f; // Velocidad de disparo en disparos por segundo
-    private float tempsDispar = 0.3f; // Tiempo para el próximo disparo
->>>>>>> 07a2eb306437b70f6a8b3edf46950416e4866bec
 
     // Update is called once per frame
     void Update()
