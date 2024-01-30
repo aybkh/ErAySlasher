279f80ea8aff35ca2d042722d04a60b8f2de5bea
aybkh <a.khalifi@sapalomera.cat>
2024-01-30 17:54:06 +0100

    1

--- ErAySlasher/Assets/Scripts/DispararBalas.cs
+++ ErAySlasher/Assets/Scripts/DispararBalas.cs
@@ -4,32 +4,24 @@ using UnityEngine;
 
 public class DispararBalas : MonoBehaviour
 {
-    public GameObject Bala; // El prefab de la bala
-    public Transform Enemic1, Enemic2; // El objeto al que se disparará
-
-    public float fireRate = 1f; // Velocidad de disparo en disparos por segundo
-    private float tempsDispar = 0f; // Tiempo para el próximo disparo
+    public GameObject Bala;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CreaBala", 0f, 2f);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        // Verificar si es tiempo de disparar
-        if (Time.time >= tempsDispar)
-        {
-            // Disparar
-            Shoot();
-            // Actualizar el tiempo para el próximo disparo
-            tempsDispar = Time.time + 1f / fireRate;
-        }
+
     }
 
-    void Shoot()
+    private void CreaBala()
     {
-        // Instanciar la bala
-        GameObject bullet = Instantiate(Bala, transform.position, Quaternion.identity) ;
-        Destroy(bullet, 2.0f);
-        Vector3 direction = (Enemic1.position - transform.position).normalized;
-        // Obtener el componente de movimiento de la bala y establecer su dirección
-        MovimentsBala bulletMovement = bullet.GetComponent<MovimentsBala>();
+
+        Destroy(Instantiate(Bala, this.transform.position, this.transform.rotation), 10.0f);
+
+
     }
 }
