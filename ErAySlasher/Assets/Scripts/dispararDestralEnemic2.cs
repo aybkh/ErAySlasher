7f6e6e90008995dd4af311a19ae27550c3ed3e3e
aybkh <a.khalifi@sapalomera.cat>
2024-02-06 17:17:57 +0100

    1

--- ErAySlasher/Assets/Scripts/dispararDestralEnemic2.cs
+++ ErAySlasher/Assets/Scripts/dispararDestralEnemic2.cs
@@ -8,7 +8,7 @@ public class dispararDestralEnemic2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("CreaBala", 0f, 4f);
+        InvokeRepeating("CreaDestral", 0f, 4f);
     }
 
     // Update is called once per frame
